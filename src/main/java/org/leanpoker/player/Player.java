cd69e8639af5bc1e700c7d4c1aa27789ff060c05
mpailer <mpailer@209.85.243.10.in-addr.arpa>
2017-02-28 11:50:10 +0100

    raise highest bid

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,9 +9,21 @@ public class Player {
 
     public static int betRequest(JsonElement request) {
     	try{
-    		JsonElement c = request.getAsJsonObject().get("players");
-    		if (c != null){
-    			return 500;
+    		JsonElement playersElement = request.getAsJsonObject().get("players");
+    		if(playersElement.isJsonArray()){
+    			JsonArray playersArray = playersElement.getAsJsonArray();
+    			
+    			int highestBid = 0;
+    			
+    			for(JsonElement player : playersArray){
+    				if(!player.getAsJsonObject().get("name").equals("Java Master Race Maybe")){
+    					if(player.getAsJsonObject().get("bet").getAsInt() > highestBid){
+    						highestBid = player.getAsJsonObject().get("bet").getAsInt();
+    					}
+    				}
+    			}
+    				
+    			return highestBid + 5;
     		}
     		
     	}

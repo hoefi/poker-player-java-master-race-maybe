162c3eb923a6ef377924e140e2ebb74b3aa54c76
mpailer <mpailer@209.85.243.10.in-addr.arpa>
2017-02-28 11:09:31 +0100

    logs

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,5 +1,6 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
 public class Player {
@@ -7,6 +8,15 @@ public class Player {
     static final String VERSION = "Default Java folding player";
 
     public static int betRequest(JsonElement request) {
+    	try{
+    		JsonArray asJsonArray = request.getAsJsonArray();
+    		for(Object o : asJsonArray){
+    			System.out.println(o.toString());
+    		}
+    	}
+    	catch(Exception e){
+    		System.out.println(e.getLocalizedMessage());
+    	}
         return 205;
     }
 
